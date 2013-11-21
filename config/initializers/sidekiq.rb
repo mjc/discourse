648332eb448eeb3fb7966489d9fa804bddd3c6ec
Michael J. Cohen <mjc@kernel.org>
2013-11-21 14:36:35 -0500

    whoops, leftover tidbit from autoscaler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,30 +1,30 @@
 sidekiq_redis = { url: $redis.url, namespace: 'sidekiq' }
 
-if Rails.env.production?
+# if Rails.env.production?
 
-  require 'autoscaler/sidekiq'
-  require 'autoscaler/heroku_scaler'
+#   require 'autoscaler/sidekiq'
+#   require 'autoscaler/heroku_scaler'
 
-    Sidekiq.configure_server do |config|
-      config.redis = sidekiq_redis
-      config.server_middleware do |chain|
-        chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new('sidekiq'), 60)
-      end
-    end
+#     Sidekiq.configure_server do |config|
+#       config.redis = sidekiq_redis
+#       config.server_middleware do |chain|
+#         chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new('sidekiq'), 60)
+#       end
+#     end
 
-    Sidekiq.configure_client do |config|
-      config.redis = sidekiq_redis
-      config.client_middleware do |chain|
-        chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new('sidekiq')
-      end
-    end
+#     Sidekiq.configure_client do |config|
+#       config.redis = sidekiq_redis
+#       config.client_middleware do |chain|
+#         chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new('sidekiq')
+#       end
+#     end
 
-else
+# else
 
   Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
   Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
 
-end
+# end
 
 
 Sidetiq.configure do |config|
